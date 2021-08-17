cc9f33ec7dc01c6430fabbeaa997fcb2c946eab6
Camille <camillegomes74@gmail.com>
2021-08-17 20:28:45 -0300

    Feat: Change Background Header event

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,3 +30,12 @@ function linkAction(){
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+function scrollHeader(){
+    const header = document.getElementById('header');
+    // When scroll is greater than 100 viewport height, add the scroll-header class
+    if(this.scrollY >= 100) header.classList.add('scroll-header');
+    else header.classList.remove('scroll-header')
+}
+
+window.addEventListener('scroll', scrollHeader);
