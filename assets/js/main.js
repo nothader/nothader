eb37ebd2a9356f90ba866607d655db6af5e81860
Camille <camillegomes74@gmail.com>
2021-08-18 10:35:16 -0300

    Feat: Play and pause click event

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,4 +54,30 @@ var swiper = new Swiper(".discover__container", {
     coverflowEffect: {
       rotate: 0,
     },
-  });
+});
+
+// VIDEO
+const videoFile = document.getElementById('video-file'),
+      videoButton = document.getElementById('video-button'),
+      videoIcon = document.getElementById('video-icon');
+
+function playPause() {
+    if (videoFile.paused) {
+        // play video
+        videoFile.play()
+
+        //change the play icon for pause icon
+        videoIcon.classList.add('ri-pause-line')
+        videoIcon.classList.remove('ri-play-line')
+    }
+    else{
+        // pause video
+        videoFile.pause()
+
+        videoIcon.classList.remove('ri-pause-line')
+        videoIcon.classList.add('ri-play-line')
+    }
+}
+
+videoButton.addEventListener('click', playPause)
+
