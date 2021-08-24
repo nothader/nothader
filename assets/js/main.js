def7f8ad1b89c07ea4b3867fde18c1f3e474ad7e
Camille <camillegomes74@gmail.com>
2021-08-23 23:06:18 -0300

    Feat: Scroll Reveal Animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -129,6 +129,28 @@ function scrollActive(){
 }
 window.addEventListener('scroll', scrollActive)
 
+// SCROLL REVEAL ANIMATION  
+const sr = ScrollReveal({
+    distance:'60px',
+    duration:2800,
+    // reset: true,
+})
+
+sr.reveal('.home__data, .home__social-link, .home__info, .discover__container, .experience__data, .experience__overlay, .place__card, .sponsor__content, .footer__data, .footer__rights',{
+    origin:'top',
+    interval: 100,
+})
+
+sr.reveal('.about__data, .video__description, .subscribe__description',{
+    origin:'left',
+})
+
+
+sr.reveal('.about__img-overlay, .video__content, .subscribe__form',{
+    origin:'right',
+    interval: 100,
+})
+
 // DARK LIGHT THEME
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
